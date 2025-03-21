8a012ab77af2b706ec2cf6fb97a6e8bc04cec2cb
Felix Canino <fcanino561@gmail.com>
2025-03-20 23:12:53 -0400

    Fix package refs in zillow-api-pipeline.py

--- zillow-api-pipeline.py
+++ zillow-api-pipeline.py
@@ -1,6 +1,9 @@
 import os
 from dotenv import load_dotenv
+from zillow_api_scripts.zillow_api_data_extraction import ZillowAPI
 
 load_dotenv()
 ZILLOW_API_KEY = os.getenv("ZILLOW_API_KEY")
 ZILLOW_API_HOST = os.getenv("ZILLOW_API_HOST")
+
+zillow = ZillowAPI(ZILLOW_API_KEY, ZILLOW_API_HOST, 3, 5)
