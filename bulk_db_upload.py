b8323d084a2249b005b360e6c4dba0922e284183
Felix Canino <fcanino561@gmail.com>
2026-04-25 22:00:52 -0400

    Add bulk_db_upload script for historical uploads

--- bulk_db_upload.py
+++ bulk_db_upload.py
@@ -1,32 +1,58 @@
-from db_scripts.db_utils import insert_zillow_listings_data
-import os
+from db_scripts.db_utils import upsert_zillow_data
 import json
+from pathlib import Path
 
-historical_listings_files = [
-    "zillow_location_search_20250321003842",
-    "zillow_location_search_20250321003952",
-    "zillow_location_search_20250417002000",
-    "zillow_location_search_20250417003402",
-    "zillow_location_search_20251229122008",
-    "zillow_location_search_20251230102041",
-    "zillow_location_search_20260120212304",
-    "zillow_location_search_20260217011412",
-    "zillow_location_search_20260217025029",
-    "zillow_location_search_20260217195522",
-    "zillow_location_search_20260220205838",
-    "zillow_location_search_20260305225110",
-    "zillow_location_search_20260307135946",
-    "zillow_location_search_20260420224058",
-    "zillow_location_search_20260422192557"
-]
+def upload_historical_data(db_table_name, file_list):
 
-for file in historical_listings_files:
-    filename = f"./temp_files/{file}.json"
-    with open(filename, "r") as f:
-        data = json.load(f)
-    property_records = []
-    for payload in data:
-        if payload != []:
-            if payload["totalResultCount"] != 0:
-                property_records.extend(payload["props"])
-    insert_zillow_listings_data(property_records, True, file)
+    ALLOWED_DB_TABLES = [
+        "zillow_listings_raw",
+        "zillow_property_details_raw"
+    ]
+
+    if db_table_name not in ALLOWED_DB_TABLES:
+        print(f"Historical upload process not supported for {db_table_name}")   
+
+    else:
+
+        for file in file_list:
+
+            print(file)
+            file_path = f"./temp_files/{file}"
+
+            with open(file_path, "r") as f:
+                data = json.load(f)
+
+            if isinstance(data, dict):
+                data = [data]
+
+            if db_table_name == "zillow_listings_raw":
+                records = []
+                for record in data:
+                    if record != []:
+                        if record["totalResultCount"] != 0:
+                            records.extend(record["props"])
+
+            elif db_table_name == "zillow_property_details_raw":
+                records = data
+
+            upsert_zillow_data(records, db_table_name, True, file)
+
+if __name__ == "__main__":
+
+    hist_directory = Path("./temp_files/")
+
+    zillow_listings_files = []
+    zillow_properties_files = []
+
+    for item in hist_directory.iterdir():
+
+        if item.is_file():
+
+            if "zillow_location_search_" in item.name:
+                zillow_listings_files.append(item.name)
+
+            elif "zillow_property_details_" in item.name:
+                zillow_properties_files.append(item.name)
+
+    upload_historical_data("zillow_listings_raw", zillow_listings_files)
+    upload_historical_data("zillow_property_details_raw", zillow_properties_files)
