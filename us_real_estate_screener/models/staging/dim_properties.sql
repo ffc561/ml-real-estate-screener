68de743bd036405380b9fa1d767319abeb7f6f16
Felix Canino <fcanino561@gmail.com>
2026-04-26 12:57:17 -0400

    Update dim_properties model with additional attributes

--- us_real_estate_screener/models/staging/dim_properties.sql
+++ us_real_estate_screener/models/staging/dim_properties.sql
@@ -3,23 +3,23 @@ WITH raw_property_details AS (
       FROM {{ source('raw_zillow', 'zillow_property_details_raw') }}
 ),
 
-listing_property_dimensions AS (
-    SELECT zpid
-         , address
-         , property_type
-         , bedrooms
-         , bathrooms
-         , latitude
-         , longitude
-         , living_area
-         , lot_area_value
-         , lot_area_unit
-         , detail_url
-         , last_extracted_at
+raw_listings AS (
+    SELECT *
+      FROM {{ source('raw_zillow', 'zillow_listings_raw') }}
+),
+
+latest_listings AS (
+    SELECT *
       FROM {{ ref('latest_zillow_listings') }}
+),
+
+all_zpids AS (
+    SELECT zpid FROM raw_property_details
+    UNION
+    SELECT zpid FROM raw_listings
 )
 
-SELECT lpd.zpid AS property_zillow_id
+SELECT az.zpid AS property_zillow_id
      , lpd.address as full_address
      , COALESCE(rpd.home_type, lpd.property_type) AS property_type
      , COALESCE(rpd.latitude, lpd.latitude) AS latitude
@@ -34,25 +34,40 @@ SELECT lpd.zpid AS property_zillow_id
      , rpd.neighborhood_region->>'name' AS parsed_neighborhood_region
      , rpd.county
      , rpd.country
-     , CASE
-           WHEN rpd.date_posted = ''
-               THEN NULL
-           ELSE rpd.date_posted :: DATE
-       END AS date_posted
-     , CASE
-           WHEN rpd.date_sold = ''
-               THEN NULL
-           ELSE rpd.date_sold :: DATE
-       END AS date_sold
      , rpd.year_built
      , rpd.attribution_info->>'mlsId' AS mls_id
      , rpd.bathrooms AS bathroom_count
      , rpd.bedrooms AS bedroom_count
-     , rpd.description AS zillow_property_description
-     , rpd.property_tax_rate
+     , COALESCE(rpd.living_area, lpd.living_area) AS living_area_sqft
+     , lpd.lot_area_value
+     , lpd.lot_area_unit
+     -- Structural Details from reso_facts
+     , rpd.reso_facts->>'architecturalStyle' AS architectural_style
+     , rpd.reso_facts->>'buildingName' AS building_name
+     , (rpd.reso_facts->>'stories')::numeric AS stories_count
+     , (rpd.reso_facts->>'parkingCapacity')::numeric AS parking_capacity
+     , (rpd.reso_facts->>'isNewConstruction')::boolean AS is_new_construction
+     , rpd.reso_facts->>'levels' AS levels_description
+     , rpd.reso_facts->>'basement' AS basement_description
+     , rpd.reso_facts->>'roofType' AS roof_type
+     , rpd.reso_facts->>'foundationDetails' AS foundation_details
+     , rpd.reso_facts->>'constructionMaterials' AS construction_materials
+     , rpd.reso_facts->>'exteriorFeatures' AS exterior_features
+     , rpd.reso_facts->>'flooring' AS flooring_types
+     , rpd.reso_facts->>'interiorFeatures' AS interior_features
+     , rpd.reso_facts->>'appliances' AS appliances_list
+     , rpd.reso_facts->>'cooling' AS cooling_systems
+     , rpd.reso_facts->>'heating' AS heating_systems
+     , (rpd.reso_facts->>'fireplaces')::numeric AS fireplace_count
+     , rpd.reso_facts->>'sewer' AS sewer_description
+     , rpd.reso_facts->>'waterSource' AS water_source
+     , rpd.reso_facts->>'zoning' AS zoning_code
+     , rpd.reso_facts->>'parcelNumber' AS parcel_number
+     -- Descriptions and Metadata
+     , rpd.description AS structural_description
      , lpd.last_extracted_at
      , rpd.created_at
      , rpd.updated_at
-  FROM listing_property_dimensions AS lpd
-       LEFT JOIN raw_property_details AS rpd
-       ON lpd.zpid = rpd.zpid
+  FROM all_zpids AS az
+       LEFT JOIN latest_listings AS lpd ON az.zpid = lpd.zpid
+       LEFT JOIN raw_property_details AS rpd ON az.zpid = rpd.zpid
