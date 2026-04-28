c4454e97cc27592c29950a04ee41513fd3867083
Felix Canino <fcanino561@gmail.com>
2026-04-27 21:19:23 -0400

    Ease guardrails in property_details_pipeline.py

--- property_details_pipeline.py
+++ property_details_pipeline.py
@@ -14,7 +14,7 @@ zpids_query = f"""
 SELECT DISTINCT zpid
   FROM public.zillow_listings_raw
  WHERE zpid NOT IN (SELECT DISTINCT zpid FROM public.zillow_property_details_raw)
- LIMIT 250
+ LIMIT 500
 """
 
 zpids_list = [record[0] for record in extract_data(zpids_query)]
