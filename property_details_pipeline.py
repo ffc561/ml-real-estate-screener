f6a206912203a0900f0f5aac4cdc34644ae37c91
Felix Canino <fcanino561@gmail.com>
2026-04-29 21:42:58 -0400

    Add more exception handling in property_details_pipeline.py

--- property_details_pipeline.py
+++ property_details_pipeline.py
@@ -14,7 +14,7 @@ zpids_query = f"""
 SELECT DISTINCT zpid
   FROM public.zillow_listings_raw
  WHERE zpid NOT IN (SELECT DISTINCT zpid FROM public.zillow_property_details_raw)
- LIMIT 500
+ LIMIT 502
 """
 
 zpids_list = [record[0] for record in extract_data(zpids_query)]
@@ -24,13 +24,20 @@ property_details_list = []
 print(f"Number of properties to extract data on: {len(zpids_list)}")
 
 for zpid in zpids_list:
+
     print(f"Property ID: {zpid}")
     detail = uhmd.get_property_details_by_zpid(zpid)
+
     if detail is not None:
-        property_details_list.append(detail)
+        if "zpid" in detail and detail["zpid"] is not None:
+            property_details_list.append(detail)
+        else:
+            print(f"Invalid record for {zpid}. Skipping")
+            print(detail)
     else:
         print(f"No result found for {zpid}.  Skipping")
-    time.sleep(2)
+
+    time.sleep(1)
 
 extraction_dt = datetime.now().strftime("%Y%m%d%H%M%S")
 
