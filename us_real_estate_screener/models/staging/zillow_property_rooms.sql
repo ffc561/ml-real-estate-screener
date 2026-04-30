6608ce25278061e5da2da78c8d54597dd3ff89de
Felix Canino <fcanino561@gmail.com>
2026-04-29 21:44:30 -0400

    Fix data types in zillow_property_rooms.sql dbt model

--- us_real_estate_screener/models/staging/zillow_property_rooms.sql
+++ us_real_estate_screener/models/staging/zillow_property_rooms.sql
@@ -1,19 +1,14 @@
 SELECT zpid
-     , jsonb_array_elements(reso_facts->'rooms')->'area' AS area
-     , jsonb_array_elements(reso_facts->'rooms')->'level' AS "level"
-     , jsonb_array_elements(reso_facts->'rooms')->'features' AS features
-     , jsonb_array_elements(reso_facts->'rooms')->'roomArea' AS room_area
-     , jsonb_array_elements(reso_facts->'rooms')->'roomType' AS room_type
-     , jsonb_array_elements(reso_facts->'rooms')->'roomLevel' AS room_level
-     , jsonb_array_elements(reso_facts->'rooms')->'roomWidth' AS room_width
-     , jsonb_array_elements(reso_facts->'rooms')->'dimensions' AS dimensions
-     , jsonb_array_elements(reso_facts->'rooms')->'roomLength' AS room_length
-     , jsonb_array_elements(reso_facts->'rooms')->'description' AS "description"
+     , jsonb_array_elements(reso_facts->'rooms')->>'roomType' AS room_type
+     , (jsonb_array_elements(reso_facts->'rooms')->>'roomArea') :: NUMERIC AS room_area
+     , jsonb_array_elements(reso_facts->'rooms')->>'roomAreaUnits' AS room_area_units
+     , jsonb_array_elements(reso_facts->'rooms')->>'roomAreaSource' AS room_area_source
      , jsonb_array_elements(reso_facts->'rooms')->'roomFeatures' AS room_features
-     , jsonb_array_elements(reso_facts->'rooms')->'roomAreaUnits' AS room_area_units
-     , jsonb_array_elements(reso_facts->'rooms')->'roomAreaSource' AS room_area_source
-     , jsonb_array_elements(reso_facts->'rooms')->'roomDimensions' AS room_dimensions
-     , jsonb_array_elements(reso_facts->'rooms')->'roomDescription' AS room_description
-     , jsonb_array_elements(reso_facts->'rooms')->'roomLengthWidthUnits' AS room_length_width_units
-     , jsonb_array_elements(reso_facts->'rooms')->'roomLengthWidthSource' AS room_length_width_source
+     , jsonb_array_elements(reso_facts->'rooms')->>'roomLevel' AS room_level
+     , (jsonb_array_elements(reso_facts->'rooms')->>'roomLength') :: NUMERIC AS room_length
+     , (jsonb_array_elements(reso_facts->'rooms')->>'roomWidth') :: NUMERIC AS room_width
+     , jsonb_array_elements(reso_facts->'rooms')->>'roomDimensions' AS room_dimensions
+     , jsonb_array_elements(reso_facts->'rooms')->>'roomLengthWidthUnits' AS room_length_width_units
+     , jsonb_array_elements(reso_facts->'rooms')->>'roomLengthWidthSource' AS room_length_width_source
+     , jsonb_array_elements(reso_facts->'rooms')->>'roomDescription' AS room_description
   FROM {{ source('raw_zillow', 'zillow_property_details_raw') }}
