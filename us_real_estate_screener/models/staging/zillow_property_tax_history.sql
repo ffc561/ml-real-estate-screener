8a9ab6055d7731f5d0b1e561cc738b1cfc452bb8
Felix Canino <fcanino561@gmail.com>
2026-05-03 14:22:29 -0400

    Fix column whitespace in tax history model

--- us_real_estate_screener/models/staging/zillow_property_tax_history.sql
+++ us_real_estate_screener/models/staging/zillow_property_tax_history.sql
@@ -8,7 +8,7 @@ SELECT zpid
      , TO_TIMESTAMP((tax_record->>'time') :: BIGINT / 1000) :: DATE AS as_of_time
      , (tax_record->>'value') :: NUMERIC(18, 2) AS tax_appraisal_value
      , (tax_record->>'taxPaid') :: NUMERIC(12, 2) AS tax_paid
-     , ((tax_record->>'taxPaid') :: NUMERIC/ (tax_record->>'value') :: NUMERIC) :: NUMERIC(24,10) AS effective_tax_rate
+     , ((tax_record->>'taxPaid') :: NUMERIC / (tax_record->>'value') :: NUMERIC) :: NUMERIC(24,10) AS effective_tax_rate
      , (tax_record->>'taxIncreaseRate') :: NUMERIC(24, 10) AS tax_increase_rate
      , (tax_record->>'valueIncreaseRate') :: NUMERIC(24, 10) AS value_increase_rate
   FROM tax_history_details
